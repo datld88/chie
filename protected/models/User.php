fc6aac33ed621fb2faf5fb12a7df4e3cc1206c78
nnlam <nguyenlam@innoflex.vn>
2012-12-27 11:20:27 +0700

    declare các relations với các model liên quan thêm hàm isAdmin(), validatePassword(), và tự generate một random string cho salt override hàm beforeSave()

--- protected/models/User.php
+++ protected/models/User.php
@@ -1,21 +1,30 @@
 <?php
 
 /**
- * This is the model class for table "chie_user".
+ * This is the model class for table "{{user}}".
  *
- * The followings are the available columns in table 'chie_user':
+ * The followings are the available columns in table '{{user}}':
  * @property integer $id
  * @property string $username
- * @property string $fullname
- * @property string $email
  * @property string $password
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $salt
  * @property integer $is_admin
+ *
+ * The followings are the available model relations:
+ * @property LogComment[] $logComments
+ * @property LogUserOther[] $logUserOthers
+ * @property LogUserPayment[] $logUserPayments
+ * @property LogUserPlaygame[] $logUserPlaygames
+ * @property UserLogRate[] $userLogRates
+ * @property UserProfile $userProfile
  */
 class User extends CActiveRecord
 {
+    const ADMIN=1;
+    const NOT_ADMIN=0;
+    
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
@@ -31,7 +40,7 @@ class User extends CActiveRecord
 	 */
 	public function tableName()
 	{
-		return 'chie_user';
+		return '{{user}}';
 	}
 
 	/**
@@ -42,15 +51,15 @@ class User extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('created_at, updated_at, is_admin', 'numerical', 'integerOnly'=>true),
-			array('username, fullname, email, password', 'length', 'max'=>200),
-			array('salt', 'length', 'max'=>100),
+			array('username, password', 'required'),
+			array('username, password', 'length', 'max'=>200),
+                        array('username', 'unique', 'className'=>'User'),
+                        array('is_admin', 'boolean'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, username, fullname, email, password, created_at, updated_at, salt, is_admin', 'safe', 'on'=>'search'),
+			array('id, username, created_at, updated_at, is_admin', 'safe', 'on'=>'search'),
 		);
 	}
-
 	/**
 	 * @return array relational rules.
 	 */
@@ -59,6 +68,12 @@ class User extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+			'logComments' => array(self::HAS_MANY, 'LogComment', 'user_id'),
+			'logUserOthers' => array(self::HAS_MANY, 'LogUserOther', 'user_id'),
+			'logUserPayments' => array(self::HAS_MANY, 'LogUserPayment', 'user_id'),
+			'logUserPlaygames' => array(self::HAS_MANY, 'LogUserPlaygame', 'user_id'),
+			'userLogRates' => array(self::HAS_MANY, 'UserLogRate', 'user_id'),
+			'userProfile' => array(self::HAS_ONE, 'UserProfile', 'user_id'),
 		);
 	}
 
@@ -70,13 +85,11 @@ class User extends CActiveRecord
 		return array(
 			'id' => 'ID',
 			'username' => 'Username',
-			'fullname' => 'Fullname',
-			'email' => 'Email',
 			'password' => 'Password',
 			'created_at' => 'Created At',
 			'updated_at' => 'Updated At',
 			'salt' => 'Salt',
-			'is_admin' => 'Is Admin',
+			'is_admin' => 'Phân Quyền Admin?',
 		);
 	}
 
@@ -90,19 +103,48 @@ class User extends CActiveRecord
 		// should not be searched.
 
 		$criteria=new CDbCriteria;
-
 		$criteria->compare('id',$this->id);
 		$criteria->compare('username',$this->username,true);
-		$criteria->compare('fullname',$this->fullname,true);
-		$criteria->compare('email',$this->email,true);
-		$criteria->compare('password',$this->password,true);
 		$criteria->compare('created_at',$this->created_at);
 		$criteria->compare('updated_at',$this->updated_at);
-		$criteria->compare('salt',$this->salt,true);
 		$criteria->compare('is_admin',$this->is_admin);
-
+                
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
 		));
 	}
+        
+        public function getAdminOptions(){
+            return array(
+                self::ADMIN =>'Admin',
+                self::NOT_ADMIN=>'Không Phải Admin',
+            );
+        }
+        
+        protected function beforeSave(){
+            if(parent::beforeSave()){
+                $salt_len=7;
+                $this->salt=$this->_randomString($salt_len);
+                $this->password=md5($this->password.$this->salt);
+                $this->updated_at=time();
+                if($this->isNewRecord)
+                    $this->created_at=time();
+                return true;
+            }
+            return false;
+        }
+        
+        protected function _randomString($len){
+            $string=array_merge(range(0,9), range('a', 'z'), range('A', 'Z'));
+            $string=implode("", $string);
+            return substr(str_shuffle($string), 0, $len);
+        }
+        
+        public function isAdmin(){
+            return ($this->is_admin==self::ADMIN);
+        }
+        
+        public function validatePassword($password){
+            return ($this->password==md5($password.$this->salt));
+        }
 }
