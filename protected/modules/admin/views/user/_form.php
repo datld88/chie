9152f30ccc2b38a18e4c77f23e997054a048d626
nnlam <nguyenlam@innoflex.vn>
2012-12-28 16:13:58 +0700

    sửa view form cho hợp với bootstrap

--- protected/modules/admin/views/user/_form.php
+++ protected/modules/admin/views/user/_form.php
@@ -1,38 +1,42 @@
-
-
-<div class="form">
+<div class="span6">
     <?php $form=$this->beginWidget('CActiveForm', array(
         'id'=>'user-form',
+        'htmlOptions'=>array('class'=>'form-horizontal'),
         'enableAjaxValidation'=>false,
     ));?>
     
-    <p class="note">Fields with <span class="required">*</span> are required.</p>
     <?php echo $form->errorSummary($model);?>
-    <div class="row">
+    
+    <div class="control-group">
         <?php 
-            echo $form->labelEx($model, 'username');
+            echo $form->labelEx($model, 'username', array('class'=>'control-label'));
+            echo '<div class="controls">';
             echo $form->textField($model, 'username');
             echo $form->error($model, 'username');
+            echo "</div>";
             ?>
     </div>
-    <div class="row">
+    <div class="control-group">
         <?php
-            echo $form->labelEx($model, 'password');
+            echo $form->labelEx($model, 'password', array('class'=>'control-label'));
+            echo '<div class="controls">';
             echo $form->passwordField($model, 'password');
             echo $form->error($model, 'password');
+            echo '</div>';
         ?>
     </div>
-    <div class="row">
+    <div class="control-group">
         <?php
-            echo $form->labelEx($model, 'is_admin');
+            echo $form->labelEx($model, 'is_admin', array('class'=>'control-label'));
+            echo '<div class="controls">';
             echo $form->dropDownList($model, 'is_admin', 
                     User::model()->getAdminOptions());
             echo $form->error($model, 'is_admin');
+            echo '</div>';
         ?>
     </div>
-    
-    <div class="row buttons">
-        <?php echo CHtml::submitButton($model->isNewRecord? 'Create':'Save');?>
+    <div class="controls">
+        <?php echo CHtml::submitButton($model->isNewRecord? 'Create': 'Save', array('class'=>'btn btn-success'));?>
     </div>
     <?php $this->endWidget();?>
 </div>
