380d24f865fa3c0c22b74db896bba7435fcb63f3
nnlam <nguyenlam@innoflex.vn>
2012-12-28 17:09:24 +0700

    sửa view form cho hợp với bootstrap

---
+++ protected/modules/admin/views/user/update.php
@@ -0,0 +1,8 @@
+<?php
+echo '<h1>Edit User: ', $model->username, '</h1>';
+$this->renderPartial('_form', array('model'=>$model));
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
