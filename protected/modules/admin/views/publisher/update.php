53c23814e1a9e8b49ddfd65b2591f4a20ac3c8dc
nnlam <nguyenlam@innoflex.vn>
2012-12-28 18:00:51 +0700

    sửa view form cho hợp với bootstrap

---
+++ protected/modules/admin/views/publisher/update.php
@@ -0,0 +1,9 @@
+<?php
+echo '<h1>Update Publisher: ', $model->name, '</h1>';
+$this->renderPartial('_form', array('model'=>$model));
+/*
+
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
