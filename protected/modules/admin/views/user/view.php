8b44860a3352a467932f1e7df772c65683fa2775
nnlam <nguyenlam@innoflex.vn>
2012-12-28 13:47:29 +0700

    sửa checkUserID trong rules()

--- protected/modules/admin/views/user/view.php
+++ protected/modules/admin/views/user/view.php
@@ -15,10 +15,11 @@
         <div class="tab-pane active row-fluid" id="tab1">
             <div class="span4">
             <?php
-                echo '<h3>Username: ', $user->username, '</h3>';
-                echo '<p>Ngày Khởi Tạo: ', date('r', $user->created_at), '</p>';
-                echo '<p>Ngày Thay Đổi Gần Nhất: ', date('r', $user->updated_at), '</p>';
-                echo '<p>Phân Quyền Admin: ', ($user->is_admin? 'Có':'Không'), '</p>';
+                $accLabels=$user->attributeLabels();
+                echo '<h3>', $accLabels['username'], ': ', $user->username, '</h3>';
+                echo '<p>', $accLabels['created_at'], ': ', date('r', $user->created_at), '</p>';
+                echo '<p>', $accLabels['updated_at'], ': ', date('r', $user->updated_at), '</p>';
+                echo '<p>', $accLabels['is_admin'], ': ', ($user->is_admin? 'Có':'Không'), '</p>';
             ?>
             </div>
             <div class="span6">
@@ -26,13 +27,22 @@
                 if(empty($user->userProfile))
                     echo '<h3>User chưa tạo profile</h3>';
                 else{
-                    echo '<h3>Full Name: ', $user->userProfile->full_name, '</h3>';
-                    echo '<div class="span4 offset12"><img src="', $user->userProfile->avatar, '" height="100" width="100">';
-                    echo '<p>Birthday: ', $user->userProfile->birthday, '</p>';
-                    //echo '<p>Giới Tính: ', UserProfile::getGender, 
+                    $profileLabel=$user->userProfile->attributeLabels();
+                    echo '<h3>Profile: </h3>';
+                    echo '<div style="border: 1px solid black;height:100px;width:100px; float:right;"><img src="', $user->userProfile->avatar_path, $user->userProfile->avatar, '" height="100" width="100"></div>';
+                    echo '<p>', $profileLabel['full_name'], ': ', $user->userProfile->full_name, '</p>';
+                    echo '<p>', $profileLabel['gender'], ': ', $user->userProfile->getGenderString(), '</p>';
+                    echo '<p>', $profileLabel['birthday'], ': ', $user->userProfile->birthday, '</p>';
+                    //echo '<p>', $profileLabel['email'], ': ', $user->userProfile->email, '</p>';
+                    echo '<address>', $profileLabel['address'], ': <br/>';
+                    echo $user->userProfile->address, '<br/>';
+                    echo '<abbr title="', $profileLabel['city_id'], '">C:</abbr> ', City::model()->findByPk($user->userProfile->city_id)->name, '<br/>';
+                    echo '<abbr title="', $profileLabel['phone'], '">P:</abbr> ', $user->userProfile->phone, '<br/>';
+                    echo '<abbr title="', $profileLabel['email'], '">M:</abbr> ', $user->userProfile->email, '<br/>';
+                    echo '</address>';
+                    echo '<p>', $profileLabel['created_at'], ': ', date('r', $user->userProfile->created_at), '</p>';
+                    echo '<p>', $profileLabel['updated_at'], ': ', date('r', $user->userProfile->updated_at), '</p>';
                 }
-                
-                
                 ?>
             </div>
             
