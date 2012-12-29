7ba77b92546853ba4c66581ec1e469fc4398e102
nnlam <nguyenlam@innoflex.vn>
2012-12-29 23:45:50 +0700

    category controller

--- protected/modules/admin/controllers/CategoryController.php
+++ protected/modules/admin/controllers/CategoryController.php
@@ -1,46 +1,69 @@
 <?php
 
-class CategoryController extends Controller
+class CategoryController extends AdminController
 {
-	public function action_form()
+	
+	public function actionIndex()
 	{
-		$this->render('_form');
-	}
-
-	public function actionAdmin()
-	{
-		$this->render('admin');
+		$model=new GameCategory('search');
+                $model->unsetAttributes();
+                if(isset($_GET['Category']))
+                    $model->attributes=$_GET['Category'];
+                
+                $this->render('index', array('model'=>$model));
 	}
 
 	public function actionCreate()
 	{
-		$this->render('create');
+            $model=new GameCategory;
+            
+            if(isset($_POST['GameCategory'])){
+                $_POST['GameCategory']=$this->cleanXss($_POST['GameCategory']);
+                $model->attributes=$_POST['GameCategory'];
+                if($model->parent_id==0)
+                    unset($model->parent_id);
+                if($model->save())
+                    $this->redirect(Yii::app()->createUrl('/admin/category/view/id/'.$model->id));
+            }
+            $this->render('create', array('model'=>$model));
 	}
-
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+        
+        public function actionView($id){
+            $model=$this->loadModel($id);
+            
+            $this->render('view', array('model'=>$model));
+        }
+        public function actionUpdate($id){
+            $model=$this->loadModel($id);
+            
+            if(isset($_POST['GameCategory'])){
+                $model->attributes=$this->cleanXSS($_POST['GameCategory']);
+                
+                if($model->parent_id==0)
+                    unset($model->parent_id);
+                if($model->save())
+                    $this->redirect(Yii::app()->createUrl('/admin/category/view/id/'.$model->id));
+            }
+            $this->render('update', array('model'=>$model));
+        }
+        public function actionDelete($id){
+            $this->loadModel($id)->delete();
+        }
+        public function loadModel($id){
+            $id=(int)$id;
+            $model=  GameCategory::model()->findByPk($id);
+            
+            if($model===null)
+                throw new CHttpException(404, 'Không tìm thấy category vừa rồi');
+            return $model;
+        }
+        
+        public function cleanXSS($data){
+            $keys=array_keys($data);
+            $p = new CHtmlPurifier();
+            foreach($keys as $key){
+                $data[$key]=$p->purify($data[$key]);
+            }
+            return $data;
+    }        
 }
