5d0b00c00b76db0435e884308bd437383ac23aa9
nnlam <nguyenlam@innoflex.vn>
2012-12-29 11:02:37 +0700

    sua view de hien thi ca games

--- protected/modules/admin/views/publisher/view.php
+++ protected/modules/admin/views/publisher/view.php
@@ -6,11 +6,43 @@
     
     <div class="tab-content">
         <div class="tab-pane active row-fluid" id="tab1">
-            
+             <?php
+             $Labels=$publisher->attributeLabels();
+            echo '<div style="border:1px solid black; height:300px;width:300px;float:right;"><img src="',
+                     $publisher->logo_path, $publisher->logo, '" height="300" width="300"></div>';
+             echo '<h3>', $Labels['name'], ': ', $publisher->name, '</h3>';
+             echo '<p>', $Labels['status'], ': ', $publisher->publisherStatusString(), '</p>';
+             echo '<p>', $Labels['is_vip'], ': ', $publisher->publisherVipString(), '</p>';
+             echo '<address>', $Labels['address'], ': </br>';
+             echo $publisher->address, '</br>';
+             echo '<abbr title="', $Labels['phone'], '">P: </abbr>', $publisher->phone, '</br>';
+             echo '<abbr title="', $Labels['hotline'], '">H: </abbr>', $publisher->hotline, '</br>';
+             echo '<abbr title="', $Labels['website'], '">W: </abbr>', $publisher->website, '</br>';
+             echo '</address>';
+             echo '<p>', $Labels['level'], ': ', $publisher->level, '</p>'; 
+             echo '<p>', $Labels['count_game'], ': ', $publisher->count_game, '</p>';
+             ?>
         </div>
         
         <div class="tab-pane row-fluid" id="tab2">
-            
+            <?php 
+                if(empty($publisher->games))
+                    echo '<h3>Publisher chưa có game nào trong hệ thống</h3>';
+                else{
+                    $this->widget('zii.widgets.grid.CGridView', array(
+                       'id'=>'game-grid',
+                        'dataProvider'=>$publisher->games,
+                        'htmlOptions'=>array('class'=>'table table-bordered table-hover'),
+                        'columns'=>array(
+                            'name',
+                            'short_description',
+                            
+                            array('class'=>'CButtonColumn'),
+                        ),
+                        'itemsCssClass'=>'table talbe-bordered talbe-hover',
+                    ));
+                }
+            ?>
         </div>
     </div>
 </div>
