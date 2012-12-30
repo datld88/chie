9fc5e3c1969bf14508d72e57e2393dbaec65ffa1
nnlam <nguyenlam@innoflex.vn>
2012-12-31 01:51:18 +0700

    CRUD Tin tức

--- protected/modules/admin/controllers/NewsController.php
+++ protected/modules/admin/controllers/NewsController.php
@@ -1,46 +1,64 @@
 <?php
 
-class NewsController extends Controller
+class NewsController extends AdminController
 {
-	public function action_form()
+	public function actionIndex()
 	{
-		$this->render('_form');
-	}
-
-	public function actionAdmin()
-	{
-		$this->render('admin');
+            $model= new News('search');
+            $model->unsetAttributes();
+            
+            if(isset($_GET['News']))
+                $model->attributes=$this->cleanXss($_GET['News']);
+            $this->render('index', array('model'=>$model));
 	}
 
 	public function actionCreate()
 	{
-		$this->render('create');
+            $model=new News;
+            
+            if(isset($_POST['News'])){
+                
+                $model->attributes=$this->cleanXss($_POST['News']);
+                if($model->save())
+                    $this->redirect(Yii::app()->createUrl('/admin/news/view/id/'.$model->id));
+            }
+            $this->render('create', array('model'=>$model));
 	}
-
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+        
+        public function actionUpdate($id){
+            $model=$this->loadModel($id);
+            
+            if(isset($_POST['News'])){
+                $model->attributes=$this->cleanXss($_POST['News']);
+                if($model->save())
+                    $this->redirect(Yii::app()->createUrl('/admin/news/view/id/'.$model->id));
+                
+            }
+            $this->render('update', array('model'=>$model));
+        }
+        public function actionDelete($id){
+            $this->loadModel($id)->delete();
+            
+            if(!isset($_GET['ajax']))
+                $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('/admin/news'));
+        }
+        public function actionView($id){
+            $model=$this->loadModel($id);
+            $this->render('view', array('model'=>$model));
+        }
+    public function cleanXSS($data){
+        $keys=array_keys($data);
+        $p = new CHtmlPurifier();
+        foreach($keys as $key){
+            $data[$key]=$p->purify($data[$key]);
+        }
+        return $data;
+    }
+    public function loadModel($id){
+        $id=(int)$id;
+            $model=News::model()->findByPk($id);
+            if($model===null)
+                throw new CHttpException(404, 'Không tìm thấy Event cần tìm');
+            return $model;
+        }
 }
